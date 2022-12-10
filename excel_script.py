673d1967de1a51e7211e8090aa1dc4d763dc1616
Yufeng Liu <yufeng.liu15@gmail.com>
2022-12-10 16:37:40 -0500

    added address functionality using googlemaps api

--- excel_script.py
+++ excel_script.py
@@ -1,7 +1,29 @@
 import pandas as pd
+import googlemaps, urllib.parse
+from datetime import datetime
 
-# imports our data.csv file
-dataFrame = pd.read_csv('data.csv')
+while True:
+    userFileName = input("Input .csv file name to read from(do not add extension):")
+    userFileName = userFileName + ".csv"
+    try:
+        dataFrame = pd.read_csv(userFileName)
+    except:
+        print("Invalid file name")
+    else:
+        break
 
-with pd.ExcelWriter("dataBook.xlsx") as writer:
+userBookName = input("Input name to save book as (do not add extension): ")
+userBookName = userBookName + '.xlsx'
+
+gmaps = googlemaps.Client(key="AIzaSyBEU7IurUphn0vFLD5bq6o3LvLcZjqanzc")
+addresses = dataFrame.Address
+googleURLAddress = []
+
+#for i in range(len(addresses)):
+googleURL = "https://www.google.com/maps/search/?api=1&query="
+for i in range(len(addresses)):
+    googleURLAddress.append(googleURL + urllib.parse.quote(addresses[i]))
+dataFrame.Address = googleURLAddress
+
+with pd.ExcelWriter(userBookName) as writer:
     dataFrame.to_excel(writer,'data')
--- webscrape_script.py
+++ webscrape_script.py
@@ -1,24 +1,21 @@
 # use google api to implement transit time and to create a url to google map of the surroundings
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import time, os, csv
+from selenium.webdriver.common.by import By # used for By.CLASS_NAME etc. 
+import os,csv # os for system('clear'), csv for writing to csv files
 
 windowsPATH = ""
 macPATH = "/Users/yufeng/chromedriver"
 driver = webdriver.Chrome(macPATH)
-#fileHandle = open("data.txt", 'w')
 
 url = []
 price = []
 numberOfBeds = []
 numberOfBaths = []
-minuteToTrain = []
+address = []
 
-#userWebsiteToScrape = input("Copy paste the URL you would like to use: ")
-userWebsiteToScrape = "https://rentals.ca/ottawa"
-driver.get(userWebsiteToScrape)
+websiteToScrape = "https://rentals.ca/ottawa"
+driver.get(websiteToScrape)
 
 totalNumOfRentals = driver.find_element(By.CLASS_NAME, "page-title__bottom-line")
 totalNumOfRentals = totalNumOfRentals.find_element(By.TAG_NAME, "p")
@@ -26,8 +23,11 @@ totalNumOfRentals = int(totalNumOfRentals.text.split(' ', 1)[0])
 
 totalPropertiesTraversed = 0
 firstTime = True
+# data retrival for properties on the same page 
 while totalPropertiesTraversed < totalNumOfRentals:
     propertiesOnCurrPage = driver.find_elements(By.CLASS_NAME, "listing-card__details")
+    
+    # data retrieval for each specific property
     for counter in range(len(propertiesOnCurrPage)):
         currProperty = propertiesOnCurrPage[counter]
 
@@ -49,8 +49,14 @@ while totalPropertiesTraversed < totalNumOfRentals:
         lowestBedIndex = bathData.text.find("BATH")
         numberOfBaths.append(bathData.text[:lowestBedIndex])
         
+        # address data
+        addressData = currProperty.find_element(By.CLASS_NAME, "listing-card__title")
+        address.append(addressData.text)
+        
         os.system("clear")
         totalPropertiesTraversed += 1
+        
+        # percentage bar
         print (f"{(totalPropertiesTraversed/totalNumOfRentals*100):.2f}%") 
     
     # next page
@@ -68,11 +74,16 @@ for i in range(totalNumOfRentals):
     propertyDictionary["Pricing"] = price[i]
     propertyDictionary["Beds"] = numberOfBeds[i]
     propertyDictionary["Baths"] = numberOfBaths[i]
+    propertyDictionary["Address"] = address[i]
     dataDictionary[i] = propertyDictionary
 
-with open("data.csv", 'w', newline='') as csvFile:
-    _fieldnames = ['url', 'Beds', 'Baths', 'Pricing']
+csvFileName = input("Input a name for the .csv file (do not add extension): ")
+csvFileName = csvFileName + '.csv'
+
+with open(csvFileName, 'w', newline='') as csvFile:
+    _fieldnames = ['url', 'Beds', 'Baths', 'Pricing', 'Address']
     writer = csv.DictWriter(csvFile, fieldnames = _fieldnames)
+    # writeheader allows for column names using fieldnames
     writer.writeheader()
     for i in range(totalNumOfRentals):
         writer.writerow(dataDictionary[i])
